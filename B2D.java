edd358ba5040fc47865d33d30fda0efa0fbface5
Cordell <cordellbonnieux@gmail.com>
2021-06-16 14:21:27 -0700

    program now works but gives incorrect results.

--- B2D.java
+++ B2D.java
@@ -15,31 +15,43 @@ public class B2D {
         Scanner input = new Scanner(System.in);
 
         // prompt user
-        System.out.print("Enter a binary number");
+        System.out.print("Enter a binary number: ");
 
         // read binary
-        String binary = input.nextLine().trim();
+        String binary = input.nextLine();
 
         // create decimal variable to store conversion
-        int decimal = 0;
+        int decimal = 1;
+
+            // check
+            System.out.println("right before the loop");
 
         // start loop; checks for validity while converting to decimal
-        for (int i = binary.length(), e = 1; i > 0; i--, e*=2) {
+        for (int i = binary.length(), e = 1; i > 0; i--, e *= 2) {
+
+                // check
+                System.out.println("entered loop");
             
             // is the current index i of binary 0?
-            if (binary.charAt(i) == 0)
+            if (binary.charAt((i - 1)) == '0')
                 continue;
             
             // is the current index i of binary 1?
-            if (binary.charAt(i) == 1)
+            if (binary.charAt((i - 1)) == '1')
                 decimal += e;
             // if it's not 1 or 0, reset the program
             else
-                main(args);
+                //main(args);
+                System.out.println("booboo");
         }
-
         // print the results to the console
         System.out.printf("the binary number %s in decimal is %d.", binary, decimal);
 
+        // end the program
+        System.exit(0);
+
+
+                    // check
+                    System.out.println("program should have terminated before this");
     }
 }
