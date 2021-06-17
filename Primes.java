c602b5f19a831d0fd759c58ee939b0a15c80959e
Cordell <cordellbonnieux@gmail.com>
2021-06-16 19:13:29 -0700

    Quite the learning experience this is becoming. I think I need to study scope in Java and mess around with this more.

--- Primes.java
+++ Primes.java
@@ -14,19 +14,53 @@ public class Primes {
         ArrayList<Integer> primes = new ArrayList<Integer>();
 
         // loop from 2 to 1000 inclusive and check for primes
-        for (int i = 2; i <= 1000; i++) {
+        for (int i = 1000; i > 1; i--) {
 
-            // if number is not prime continue
-            if (i < 10 && (i != 2 || ))
-            else if (i % 2 == 0 || i % 3 == 0 || i % 4 == 0 || i % 5 == 0 || i % 6 == 0 || i % 7 == 0 || i % 8 == 0 || i % 9 == 0) 
-                continue;
-            // if number is prime, add to primes
-            else
+            if (!(i % 2 == 0 || i % 3 == 0 || i % 4 == 0 || i % 5 == 0 || i % 6 == 0 || i % 7 == 0 || i % 8 == 0 || i % 9 == 0))
                 primes.add(i);
+            else if (i == 2 || i == 3 || i == 5 || i == 7)
+                primes.add(i);
+
         }
 
-        for (int x = primes.size() - 1; x > 0; x--)
-            System.out.print(primes(x) + " ");
+        int size = primes.size();
+        int remainder = primes.size() % 8;
+        int groups = (primes.size() / 8) + (remainder > 0 ? 1 : 0);
+
+        while (groups > 0) {
+
+            int one, two, three, four, five, six, seven, eight;
+
+            for (int x = 0; x <= 8; x++) {
+
+                if (primes.size() > 0) {
+                    switch(x) {
+                        case 1:
+                            one = (int)primes.get(x);
+                        case 2:
+                            two = (int)primes.get(x);
+                        case 3:
+                            three = (int)primes.get(x);
+                        case 4:
+                            four = (int)primes.get(x);
+                        case 5:
+                            five = (int)primes.get(x);
+                        case 6:
+                            six = (int)primes.get(x);
+                        case 7:
+                            seven = (int)primes.get(x);
+                        case 8:
+                            eight = (int)primes.get(x);
+                    }
+                    primes.remove(x);
+                    x--;
+                }
+
+            }
+
+            System.out.printf("%d %d %d %d %d %d %d %d", one, two, three, four, five, six, seven, eight);
+        }
+    
     
     }
 }
